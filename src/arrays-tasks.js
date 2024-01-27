dbd00b09c3d5dd49e130840285b0d10e48dc18d4
unknown <sh.rusell@gmail.com>
2024-01-27 22:44:36 +0500

    feat:add propagateItemsByPositionIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -524,8 +524,9 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (arr.length < 2) return arr;
+  return arr.flatMap((el, i) => Array(i + 1).fill(el));
 }
 
 /**
